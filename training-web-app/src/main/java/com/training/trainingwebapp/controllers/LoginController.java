1b72de0d02cc2e52827270888e6d7488d84c4139
Ray <raymond.champion@att.net>
2021-03-04 20:32:46 -0600

    Added LoginController .java.  Added resource element to pom.xml to fix issue of application.properties being excluded.  Added jstl libray so that jsps commpile.

--- training-web-app/src/main/java/com/training/trainingwebapp/controllers/LoginController.java
+++ training-web-app/src/main/java/com/training/trainingwebapp/controllers/LoginController.java
@@ -10,17 +10,13 @@ import org.springframework.web.servlet.ModelAndView;
 public class LoginController {
 	
 	@RequestMapping("/login.html")
-	public ModelAndView loginController() {
-		
-		//1. get username and password
-		//2. Check that user exists - simulate a db call
-		//3. if user exists and password is correct then allow user to go to home page
-		// 		of if there is an error then notify the user that an error logging in has occurred
+	public ModelAndView login() {
 		
 		Date dateAndTime = new Date();
 		
 		return new ModelAndView("login", "dateAndTime", dateAndTime);
-		//return new ModelAndView("/WEB-INF/jsp/login.jsp", "dateAndTime", dateAndTime);
+		
+		
 		
 	}
 
